10a2c685a9c51ea1cacb193fcabbfd1e77fd7062
govindkumawat123 <govindkumawat.github@gmail.com>
2025-04-18 10:52:03 +0530

    Design changes

---
+++ src/app/components/author/session-management/live-session/live-session.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-session',
+  standalone: true,
+  imports: [],
+  templateUrl: './live-session.component.html',
+  styleUrl: './live-session.component.css'
+})
+export class LiveSessionComponent {
+
+}
